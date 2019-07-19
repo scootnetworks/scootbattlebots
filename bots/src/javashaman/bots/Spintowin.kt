991c6393c4a98b98ea7236e6f460b389bb26cefb
christian <christian@cellshift.com>
2019-07-19 16:51:49 -0700

    variable power

--- bots/src/javashaman/bots/Spintowin.kt
+++ bots/src/javashaman/bots/Spintowin.kt
@@ -4,14 +4,14 @@ import robocode.*
 class Spintowin : Robot() {
     override fun run() {
         while (true) {
-            turnGunRight(10.0)
+            turnGunRight(5.0)
         }
     }
 
     override fun onScannedRobot(event: ScannedRobotEvent?) {
         val botDistance = event?.distance ?: 10000.0
-        if (botDistance < 100.0) {
-            fire(50.0)
+        if (botDistance < 1000.0) {
+            fire(400.0 / botDistance)
         }
     }
 }
