0452f80bbc039bc345fbc9ce743fc88f8b092c06
christian <christian@cellshift.com>
2019-07-19 17:08:02 -0700

    chaos

--- bots/src/javashaman/bots/Spintowin.kt
+++ bots/src/javashaman/bots/Spintowin.kt
@@ -5,13 +5,22 @@ class Spintowin : Robot() {
     override fun run() {
         while (true) {
             turnGunRight(5.0)
+            val randomValues = List(6) { Random.nextDouble(0, 100) }
+
+            ahead(randomValues[0])
+            turnGunRight(randomValues[1])
+            ahead(randomValues[2])
+            turnGunRight(randomValues[3])
+            ahead(randomValues[4])
+            turnGunRight(randomValues[5])
+            ahead(randomValues[6])
         }
     }
 
     override fun onScannedRobot(event: ScannedRobotEvent?) {
         val botDistance = event?.distance ?: 10000.0
         if (botDistance < 1000.0) {
-            fire(400.0 / botDistance)
+            fire(1000.0 / botDistance)
         }
     }
 }
